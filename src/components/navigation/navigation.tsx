8a789b0f36006216d5dcd800767a3186ef66b7e3
MihaelaO29 <opriamihaela22@yahoo.com>
2024-07-15 11:22:02 +0300

    changed navigation style

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,5 +1,6 @@
 import './navigation.css';
 import { Link, useLocation } from 'react-router-dom';
+import logo from '../../images/logo.png';
 
 
 function Navigation() {
@@ -7,9 +8,11 @@ function Navigation() {
   return (
     <div className='nav_section'>
       <div className='nav'>
-        <div className='logo'>PORTFOLIO</div>
+        {/* <div className='logo'>PORTFOLIO</div> */}
+      
+        <Link className={`menu_link ${location?.pathname === '/' ? 'clicked' : ''}`} to='/'>  <img  className='logo' src={logo} alt='logo'/></Link>
         <div className='nav_links'>
-          <Link className={`menu_link ${location?.pathname === '/' ? 'clicked' : ''}`} to='/'>Home</Link>
+        
           <Link className={`menu_link ${location?.pathname === '/about' ? 'clicked' : ''}`} to='/about'>About</Link>
           <Link className={`menu_link ${location?.pathname === '/projects' ? 'clicked' : ''}`} to='/projects'>Projects</Link>
           <Link className={`menu_link ${location?.pathname === '/contact' ? 'clicked' : ''}`} to='/contact'>Contact</Link>
