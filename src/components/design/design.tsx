2db7fd9e7e8e3ebf43b4a6e177bd84a4d2cd5ad8
MihaelaO29 <opriamihaela22@yahoo.com>
2025-02-16 17:00:12 +0200

    Fixed the design page style

--- src/components/design/design.tsx
+++ src/components/design/design.tsx
@@ -4,30 +4,31 @@ import { useNavigate } from 'react-router-dom';
 
 function Design() {
 
-        const navigate = useNavigate();  // Hook-ul pentru navigare
-      
-        const handleButtonClick = () => {
-          navigate('/design-project');  
-        };
+    const navigate = useNavigate();  // Hook-ul pentru navigare
+
+    const handleButtonClick = () => {
+        navigate('/design-project');
+    };
 
     return (
         <div className='design_section'>
             <div className='design_section_container'>
-            <div onClick={handleButtonClick} className='design_container'>
+                <h1>UI/UX Projects</h1>
+                <div onClick={handleButtonClick} className='design_container'>
 
-                <div className='design_text'>
-                    <div className='ux_project'>
-                        <div className='ux_project_title'>Panni Bakery</div>
-                        <div className='ux_project_description'>Discover a local artisan bakery and easily explore freshly baked sourdough products.</div>
-                        <button onClick={handleButtonClick} className='ux_project_button'>View Case Study</button>
+                    <div className='design_text'>
+                        <div className='ux_project'>
+                            <div className='ux_project_title'>Panni Bakery</div>
+                            <div className='ux_project_description'>Discover a local artisan bakery and easily explore freshly baked sourdough products.</div>
+                            <button onClick={handleButtonClick} className='ux_project_button'>View Case Study</button>
+                        </div>
+                    </div>
+
+                    <div className='project_design_img'>
+                        <img className='project_cover_img' src="https://i.postimg.cc/vTrtFm4y/black-background.jpg" />
                     </div>
                 </div>
-                
-                <div className='project_design_img'>
-                    <img className='project_cover_img' src="https://i.postimg.cc/vTrtFm4y/black-background.jpg"/>
-                </div>
             </div>
-        </div>
         </div>
     )
 }
