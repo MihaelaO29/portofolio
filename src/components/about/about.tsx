1eed76ff2626c2a1ed27009639175b04e15b1665
MihaelaO29 <opriamihaela22@yahoo.com>
2025-02-17 11:58:29 +0200

    Mesajul de commit pentru modificările tale

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -84,15 +84,14 @@ function About() {
           About Me
         </div>
         <div className='about_description'>
-          Hi, I'm Mihaela! I'm a passionate Frontend Developer with a dedication to crafting visually
+          Hi, I'm Mihaela! I'm a passionate UX/UI Designer & Front-end Developer with a dedication to crafting visually
           stunning and user-friendly web experiences. My journey in web development began with a simple
           curiosity about how websites work, and has since evolved into a deep commitment to building intuitive,
           responsive interfaces that delight users.
           I have a serious passion for UI effects, animations, and creating dynamic user experiences that not
-          only look great but also feel intuitive. My goal is to blend aesthetic design with seamless functionality,
-          ensuring every project I work on is both beautiful and easy to use.<br />
-          Thank you for visiting my portfolio! I look forward to the opportunity to collaborate and create something
-          amazing together.
+          only look great but also feel intuitive.<br />
+         <p className='thank_you_text'> Great design happens when creativity meets purpose. <br/>Thank you for visiting my portfolio — I’d love to collaborate and build something extraordinary together!
+          </p>
           <br />
         </div>
       </div>
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -102,7 +102,7 @@ function Home() {
                         </div>
                     </div>
 
-                    <div className='home_content_description'> UX/UI Designer & Front-end developer</div>
+                    <div className='home_content_description'> UX/UI Designer & Front-end Developer</div>
                 </div>
             </div>
         </div>
